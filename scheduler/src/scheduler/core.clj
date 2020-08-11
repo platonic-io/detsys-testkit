ed770855c0ed3fe7f2c002b29248e5641d338cce
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2020-08-11 17:29:01 +0200

    Add restart-server.

--- scheduler/src/scheduler/core.clj
+++ scheduler/src/scheduler/core.clj
@@ -7,20 +7,31 @@
 
 (set! *warn-on-reflection* true)
 
-(defn run-server
+(defonce server (atom nil))
+
+(defn- start-server
   [port]
-  (server/run-server (-> #'handler/app
-                         (rmd/wrap-defaults rmd/api-defaults)
-                         rmr/wrap-reload)
-                     {:port port}))
+  (reset! server (server/run-server (-> #'handler/app
+                                        (rmd/wrap-defaults rmd/api-defaults)
+                                        rmr/wrap-reload)
+                                    {:port port})))
+
+(defn- stop-server
+  []
+  (when @server (@server))
+  (reset! server nil))
+
+(defn- restart-server
+  []
+  (stop-server)
+  (start-server))
 
 (defn -main
   [& _args]
   (let [port (Integer/parseInt (or (System/getenv "SCHEDULER_PORT") "3000"))]
-    (run-server port)
+    (start-server port)
     (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
 
 (comment
-(defonce server (run-server 3000))
-(server)
-)
+  (start-server 3000)
+  (stop-server))
