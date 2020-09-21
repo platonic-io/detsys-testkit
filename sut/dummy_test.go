3539917e70ccd04663abc32b57bfb1afa977fcb8
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2020-09-21 15:23:45 +0200

    Add setup and teardown.

--- sut/dummy_test.go
+++ sut/dummy_test.go
@@ -2,13 +2,40 @@ package sut
 
 import (
 	"testing"
+	"time"
 
 	"github.com/symbiont-io/detsys/executor"
 	"github.com/symbiont-io/detsys/lib"
 )
 
+var quit chan struct{}
+
+func setup() {
+	quit = make(chan struct{})
+	go loop()
+}
+
+func teardown() {
+	close(quit)
+}
+
+func loop() {
+	for {
+		select {
+		case <-quit:
+			return
+		default:
+			executor.Deploy(map[string]lib.Reactor{
+				"node1": &Node{},
+				"node2": &Node{}})
+		}
+	}
+}
+
 func TestDummy(t *testing.T) {
-	executor.Deploy(map[string]lib.Reactor{"node1": &Node{}, "node2": &Node{}})
+	setup()
+	time.Sleep(3 * time.Second)
+	teardown()
 	// lib.Execute(1)
 	// lib.Check
 }
