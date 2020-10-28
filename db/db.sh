1c4c6173c40c2fc6682b365dadbdaeefc82caea8
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2020-10-28 12:21:14 +0100

    Add db reset (drop tables and init).

--- db/db.sh
+++ db/db.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
 # Change directory to the source directory of this script. Taken from:
 # https://stackoverflow.com/a/246128/3858681
 pushd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" > /dev/null
@@ -16,6 +21,8 @@ function display_help() {
 case "${CMD}" in
     init)        sqlite3 "${DB}" < init.sql ;;
     drop_tables) sqlite3 "${DB}" < drop_tables.sql ;;
+    reset)       sqlite3 "${DB}" < drop_tables.sql
+                 sqlite3 "${DB}" < init.sql ;;
     *)           display_help ;;
 esac
 
