56b5cb4b030e9482cfce13afc3fb054ff6508c4a
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2020-12-02 14:56:50 +0100

    Add fake home directory for darwin.

--- src/checker/default.nix
+++ src/checker/default.nix
@@ -24,9 +24,14 @@ in stdenv.mkDerivation rec {
     echo "compiling lock fix workaround"
     javac java/src/lockfix/LockFix.java -cp $CLASSPATH -d $builddir
 
+    # On Darwin `clj` tries to create some folder in the home directory...
+    ${lib.optionalString stdenv.isDarwin ''
+    export HOME=$TMP/home
+    mkdir -p $HOME
+    ''}
+
     echo "compiling clojure sources"
     clj -Scp src:$CLASSPATH:$builddir \
-      -Srepro \
       -J-Djava.awt.headless=true \
       -J-Dclojure.compile.path=$builddir \
       -M -e "(compile (quote ${pname}.core))"
