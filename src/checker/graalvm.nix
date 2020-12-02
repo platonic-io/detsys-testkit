3d8851956af0f16fb31a5c7732a03659dd2327d5
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2020-12-02 13:39:39 +0100

    Remove workaround for darwin.

--- src/checker/graalvm.nix
+++ src/checker/graalvm.nix
@@ -109,19 +109,10 @@ let
             done
           '';
           "8-darwin-amd64" = ''
-            # BUG workaround http://mail.openjdk.java.net/pipermail/graal-dev/2017-December/005141.html
-            substituteInPlace $out/jre/lib/security/java.security \
-              --replace file:/dev/random    file:/dev/./urandom \
-              --replace NativePRNGBlocking  SHA1PRNG
-
             # allow using external truffle-api.jar and languages not included in the distrubution
             rm $out/jre/lib/jvmci/parentClassLoader.classpath
           '';
           "11-darwin-amd64" = ''
-            # BUG workaround http://mail.openjdk.java.net/pipermail/graal-dev/2017-December/005141.html
-            substituteInPlace $out/conf/security/java.security \
-              --replace file:/dev/random    file:/dev/./urandom \
-              --replace NativePRNGBlocking  SHA1PRNG
           '';
         }.${javaVersionPlatform};
 
