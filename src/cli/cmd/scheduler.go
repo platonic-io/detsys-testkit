7635ba0530025633f352eb8ac767d7038beb0118
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2020-12-17 12:37:03 +0100

    Protect from being able to start multiple schedulers.

--- src/cli/cmd/scheduler.go
+++ src/cli/cmd/scheduler.go
@@ -11,13 +11,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const pidFile = "detsys-scheduler.pid"
-
 var schedulerCmd = &cobra.Command{
 	Use:   "scheduler [command]",
 	Short: "Start or stop the scheduler",
 	Long:  ``,
 	Args:  cobra.ExactArgs(1),
+}
+
+func pidFile() string {
+	return filepath.Join(os.TempDir(), "detsys-scheduler.pid")
 }
 
 var schedulerUpCmd = &cobra.Command{
@@ -26,20 +28,39 @@ var schedulerUpCmd = &cobra.Command{
 	Long:  ``,
 	Args:  cobra.NoArgs,
 	Run: func(_ *cobra.Command, args []string) {
+		pid, err := readPid()
+		if err == nil {
+			fmt.Printf("Already running on pid: %d (%s)\n", pid, pidFile())
+			os.Exit(1)
+		}
+
 		cmd := exec.Command("detsys-scheduler")
 
-		err := cmd.Start()
+		err = cmd.Start()
 
 		if err != nil {
 			fmt.Printf("%s\n", err)
 			os.Exit(1)
 		}
 
-		pid := []byte(strconv.Itoa(cmd.Process.Pid))
+		pidBs := []byte(strconv.Itoa(cmd.Process.Pid))
 
-		ioutil.WriteFile(filepath.Join(os.TempDir(), pidFile), pid, 0600)
-		fmt.Printf("%s\n", pid)
+		ioutil.WriteFile(pidFile(), pidBs, 0600)
+		fmt.Printf("%s\n", pidBs)
 	},
+}
+
+func readPid() (int, error) {
+	bs, err := ioutil.ReadFile(pidFile())
+	if err != nil {
+		return -1, err
+	}
+
+	pid, err := strconv.Atoi(string(bs))
+	if err != nil {
+		return -1, err
+	}
+	return pid, nil
 }
 
 var schedulerDownCmd = &cobra.Command{
@@ -48,13 +69,8 @@ var schedulerDownCmd = &cobra.Command{
 	Long:  ``,
 	Args:  cobra.NoArgs,
 	Run: func(_ *cobra.Command, args []string) {
-		bs, err := ioutil.ReadFile(filepath.Join(os.TempDir(), pidFile))
-		if err != nil {
-			fmt.Printf("%s\n", err)
-			os.Exit(1)
-		}
 
-		pid, err := strconv.Atoi(string(bs))
+		pid, err := readPid()
 		if err != nil {
 			fmt.Printf("%s\n", err)
 			os.Exit(1)
@@ -67,6 +83,12 @@ var schedulerDownCmd = &cobra.Command{
 		}
 
 		err = process.Kill()
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			os.Exit(1)
+		}
+
+		err = os.Remove(pidFile())
 		if err != nil {
 			fmt.Printf("%s\n", err)
 			os.Exit(1)
