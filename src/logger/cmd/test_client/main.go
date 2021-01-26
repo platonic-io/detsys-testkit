4b952c4b3e281f62e4b8fc1e256b83b651320238
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2021-01-26 16:20:28 +0100

    test(logger): add test client

---
+++ src/logger/cmd/test_client/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func log(w *bufio.Writer, event []byte, meta []byte, data []byte) {
+	entry := append(bytes.Join([][]byte{event, meta, data}, []byte("\t")), byte('\n'))
+	fmt.Printf("log: entry = %s", string(entry))
+	_, err := w.Write(entry)
+	if err != nil {
+		panic(err)
+	}
+	if err := w.Flush(); err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	namedPipe := filepath.Join(os.TempDir(), "detsys-logger")
+	fh, err := os.OpenFile(namedPipe, os.O_WRONLY, 0600)
+	defer fh.Close()
+	if err != nil {
+		panic(err)
+	}
+	w := bufio.NewWriter(fh)
+
+	for i := 0; i < 129; i++ {
+		if i%20 == 0 {
+			fmt.Printf("Writing... i = %d\n", i)
+
+		}
+		log(w, []byte("TestEvent"),
+			[]byte(`{"component":"test_client", "test_id":0, "run_id":0}`),
+			[]byte(fmt.Sprintf(`{"args":{"index": %d}}`, i)),
+		)
+	}
+}
