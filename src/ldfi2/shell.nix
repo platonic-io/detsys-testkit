8f98a4a5587d1b31c67687cbb927f6381de21b94
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2021-02-12 10:29:37 +0100

    nix(ldfi2): pin nixpkgs in shell to the ones pinned by niv

--- src/ldfi2/shell.nix
+++ src/ldfi2/shell.nix
@@ -1,2 +1,5 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8103" }:
+{ sources ? import ./../../nix/sources.nix
+, nixpkgs ? import sources.nixpkgs {}
+, compiler ? "ghc8103" }:
+
 (import ./default.nix { inherit nixpkgs compiler; }).env
