5707712e67f4a4691a35487cd1952a17c673fabc
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2021-03-22 15:50:54 +0100

    test(sut/register): add asserts about expected runs and faults needed

--- src/sut/register/example_test.go
+++ src/sut/register/example_test.go
@@ -1,8 +1,10 @@
 package sut
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"reflect"
 	"testing"
 	"time"
 
@@ -38,7 +40,7 @@ func once(newFrontEnd func() lib.Reactor, testId lib.TestId, runEvent lib.Create
 	return runId, result
 }
 
-func testRegisterWithFrontEnd(newFrontEnd func() lib.Reactor, tickFrequency float64, t *testing.T) {
+func testRegisterWithFrontEnd(newFrontEnd func() lib.Reactor, tickFrequency float64, t *testing.T, expectedRuns int, expectedFaults []lib.Fault) {
 	agenda := []lib.ScheduledEvent{
 		lib.ScheduledEvent{
 			At:   time.Unix(0, 0).UTC(),
@@ -68,7 +70,9 @@ func testRegisterWithFrontEnd(newFrontEnd func() lib.Reactor, tickFrequency floa
 		Crashes: 0,
 		EOT:     0,
 	}
+	neededRuns := 0
 	for {
+		neededRuns++
 		lib.Reset()
 		runEvent := lib.CreateRunEvent{
 			Seed:          lib.Seed(4),
@@ -80,13 +84,27 @@ func testRegisterWithFrontEnd(newFrontEnd func() lib.Reactor, tickFrequency floa
 		log.Printf("Injecting faults: %#v\n", faults)
 		runId, result := once(newFrontEnd, testId, runEvent, t)
 		if !result {
-			t.Errorf("%+v and %+v doesn't pass analysis", testId, runId)
-			t.Errorf("faults: %#v\n", faults)
+			fmt.Printf("%+v and %+v doesn't pass analysis\n", testId, runId)
+			if !reflect.DeepEqual(faults, expectedFaults) {
+				t.Errorf("Expected faults:\n%#v, but got faults:\n%#v\n",
+					expectedFaults, faults)
+			}
 			break
 		}
 		faults = lib.Ldfi(testId, nil, failSpec).Faults
 		if len(faults) == 0 {
 			break
+		}
+	}
+	if !reflect.DeepEqual(faults, []lib.Fault{}) {
+		if neededRuns != expectedRuns {
+			t.Errorf("Expected to find counterexample in %d runs, but it took %d runs",
+				expectedRuns, neededRuns)
+		}
+	} else {
+		if neededRuns != expectedRuns {
+			t.Errorf("Expected to find no counterexamples in %d runs, but it took %d runs",
+				expectedRuns, neededRuns)
 		}
 	}
 }
@@ -99,7 +117,14 @@ func testRegisterWithFrontEnd(newFrontEnd func() lib.Reactor, tickFrequency floa
 // value, we expect to find a counterexample where we first update one of the
 // registers and then read from the other giving us a stale value.
 func TestRegister1(t *testing.T) {
-	testRegisterWithFrontEnd(func() lib.Reactor { return NewFrontEnd() }, 5000.0, t)
+	testRegisterWithFrontEnd(func() lib.Reactor { return NewFrontEnd() }, 5000.0, t,
+		3,
+		[]lib.Fault{
+			lib.Fault{
+				Kind: "omission",
+				Args: lib.Omission{From: "frontend", To: "register2", At: 3},
+			},
+		})
 }
 
 // In the next version of the frontend we try to avoid the above problem by
@@ -116,15 +141,30 @@ func TestRegister1(t *testing.T) {
 // write has been persisted. The client then tries to read the value and gets
 // a stale value again.
 func TestRegister2(t *testing.T) {
-	testRegisterWithFrontEnd(func() lib.Reactor { return NewFrontEnd2() }, 1000.0, t)
+	testRegisterWithFrontEnd(func() lib.Reactor { return NewFrontEnd2() }, 1000.0, t,
+		5,
+		[]lib.Fault{
+			lib.Fault{
+				Kind: "omission",
+				Args: lib.Omission{From: "frontend", To: "register1", At: 7}},
+			lib.Fault{
+				Kind: "omission",
+				Args: lib.Omission{From: "frontend", To: "register2", At: 3}},
+			lib.Fault{
+				Kind: "omission",
+				Args: lib.Omission{From: "frontend", To: "register2", At: 7}}})
 }
 
 // In the third version the above problem is fixed and the test passes.
 func TestRegister3(t *testing.T) {
-	testRegisterWithFrontEnd(func() lib.Reactor { return NewFrontEnd3() }, 1000.0, t)
+	testRegisterWithFrontEnd(func() lib.Reactor { return NewFrontEnd3() }, 1000.0, t,
+		10,
+		[]lib.Fault{})
 }
 
 // The final version is a variant of the third where timers are used instead of ticks.
 func TestRegister4(t *testing.T) {
-	testRegisterWithFrontEnd(func() lib.Reactor { return NewFrontEnd4() }, 10*1000*1000.0, t)
+	testRegisterWithFrontEnd(func() lib.Reactor { return NewFrontEnd4() }, 10*1000*1000.0, t,
+		9,
+		[]lib.Fault{})
 }
