969c43d113413deff87c1367ab93953382bd067b
Daniel Gustafsson <daniel.gustafsson@symbiont.io>
2021-04-14 13:59:42 +0200

    feat(lib): Get the reason for LTL checker

--- src/lib/ltl.go
+++ src/lib/ltl.go
@@ -9,7 +9,12 @@ import (
 	"time"
 )
 
-func LtlChecker(testId TestId, runId RunId, formula string) bool {
+type LTLResult struct {
+	Result bool   `json:"result"`
+	Reason string `json:"reason"`
+}
+
+func LtlChecker(testId TestId, runId RunId, formula string) LTLResult {
 	start := time.Now()
 	cmd := exec.Command("detsys-ltl", "check", "--testId", strconv.Itoa(testId.TestId), "--runId", strconv.Itoa(runId.RunId), "--formula", formula)
 	cmd.Stderr = os.Stderr
@@ -19,13 +24,11 @@ func LtlChecker(testId TestId, runId RunId, formula string) bool {
 	if err != nil {
 		panic(err)
 	}
+	var result LTLResult
 
-	var result struct {
-		Result bool `json:"result"`
-	}
 	err = json.Unmarshal(out, &result)
 	elapsed := time.Since(start)
 	log.Printf("LTL Checker time: %v\n", elapsed)
 
-	return result.Result
+	return result
 }
