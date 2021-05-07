311b35fa98f5488c7d954c2f88f13f6f755db286
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2021-05-07 15:07:20 +0200

    fix(runtime): namespace the event loops on the name of the tests
    Otherwise when tests run concurrently they might fail because the
    reuse the same named pipes. This problem was found by Daniel, thanks!

--- src/runtime-prototype/test/StuntDouble/EventLoopTest.hs
+++ src/runtime-prototype/test/StuntDouble/EventLoopTest.hs
@@ -18,55 +18,59 @@ testActor2 rref (Message "init") = do
   a <- remoteCall rref (Message "hi")
   return (Later a (\(Message msg) -> return (Now (Message ("Got: " ++ msg)))))
 
-eventLoopA :: EventLoopName
-eventLoopA = EventLoopName "event-loop-a"
+eventLoopA :: String -> EventLoopName
+eventLoopA suffix = EventLoopName ("event-loop-a" ++ "-" ++ suffix)
 
-eventLoopB :: EventLoopName
-eventLoopB = EventLoopName "event-loop-b"
+eventLoopB :: String -> EventLoopName
+eventLoopB suffix = EventLoopName ("event-loop-b" ++ "-" ++ suffix)
 
 ------------------------------------------------------------------------
 
 unit_invoke :: Assertion
 unit_invoke = do
   elog <- emptyEventLog
-  el <- makeEventLoop "/tmp" eventLoopA elog
+  let ev = eventLoopA "invoke"
+  el <- makeEventLoop "/tmp" ev elog
   lref <- spawn el testActor
   reply <- invoke el lref (Message "hi")
   reply @?= Message "bye!"
   l <- fmap (filter (not . isComment)) (eventLog el)
   quit el
   l @?=
-    [LogInvoke (dummyDeveloperRef el) lref (Message "hi") (Message "bye!") eventLoopA]
+    [LogInvoke (dummyDeveloperRef el) lref (Message "hi") (Message "bye!") ev]
 
 unit_send :: Assertion
 unit_send = do
   elog <- emptyEventLog
-  el <- makeEventLoop "/tmp" eventLoopA elog
+  let ev = eventLoopA "send"
+  el <- makeEventLoop "/tmp" ev elog
   catch
     (do lref <- spawn el testActor
-        let rref = localToRemoteRef (EventLoopName "event-loop-a") lref
+        let rref = localToRemoteRef ev lref
         a <- send el rref (Message "hi")
         reply <- wait a
         reply @?= Message "bye!"
         l <- fmap (filter (not . isComment)) (eventLog el)
         quit el
         l @?=
-          [ LogSendStart (dummyDeveloperRef el) rref (Message "hi") 0 eventLoopA
-          , LogRequest (dummyDeveloperRef el) rref (Message "hi") (Message "bye!") eventLoopA
-          , LogReceive rref (dummyDeveloperRef el) (Message "bye!") 0 eventLoopA
-          , LogSendFinish (CorrelationId 0) (Message "bye!") eventLoopA
+          [ LogSendStart (dummyDeveloperRef el) rref (Message "hi") 0 ev
+          , LogRequest (dummyDeveloperRef el) rref (Message "hi") (Message "bye!") ev
+          , LogReceive rref (dummyDeveloperRef el) (Message "bye!") 0 ev
+          , LogSendFinish (CorrelationId 0) (Message "bye!") ev
           ])
     (\(e :: SomeException) -> dump el >> eventLog el >>= mapM_ print)
 
 unit_sendLater :: Assertion
 unit_sendLater = do
   elog <- emptyEventLog
-  el1 <- makeEventLoop "/tmp" eventLoopA elog
-  el2 <- makeEventLoop "/tmp" eventLoopB elog
+  let evA = eventLoopA "sendLater"
+      evB = eventLoopB "sendLater"
+  el1 <- makeEventLoop "/tmp" evA elog
+  el2 <- makeEventLoop "/tmp" evB elog
 
   lref1 <- spawn el1 testActor
-  lref2 <- spawn el2 (testActor2 (localToRemoteRef eventLoopA lref1))
-  a <- send el2 (localToRemoteRef eventLoopB lref2) (Message "init")
+  lref2 <- spawn el2 (testActor2 (localToRemoteRef evA lref1))
+  a <- send el2 (localToRemoteRef evB lref2) (Message "init")
   reply <- wait a
   reply @?= Message "Got: bye!"
 
