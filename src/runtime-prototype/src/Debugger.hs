a777458252812d7ab880a04be983be91c95e7eb4
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2021-10-01 15:12:54 +0200

    feat(runtime): make debugger be able to request the log from the event loop

--- src/runtime-prototype/src/Debugger.hs
+++ src/runtime-prototype/src/Debugger.hs
@@ -1,4 +1,25 @@
 module Debugger where
 
+import Control.Concurrent.Async
+import System.FilePath
+import System.IO
+import System.Posix.Files
+
+import StuntDouble
+
+------------------------------------------------------------------------
+
+readLog :: IO Log
+readLog = do
+  let pipe = "/tmp" </> "scheduler-admin"
+  -- NOTE: We need to start reading the response before making the request to
+  -- dump the log, otherwise the response will be written to the void.
+  a <- async (withFile (pipe <> "-response") ReadWriteMode hGetLine)
+  appendFile pipe "AdminDumpLog\n"
+  s <- wait a
+  return (read s)
+
 main :: IO ()
-main = putStrLn "debugger"
+main = do
+  l <- readLog
+  print l
