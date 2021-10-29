efd420685fc0d1101f49ee154e62a244dffdcf3a
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2021-10-29 09:52:11 +0200

    refactor(runtime): clean up benchmarks a bit more, especially mp cases

--- src/runtime-prototype/bench/disruptor/SP/UnagiChan.hs
+++ src/runtime-prototype/bench/disruptor/SP/UnagiChan.hs
@@ -1,10 +1,9 @@
 module Main where
 
 import Control.Concurrent (threadDelay)
-import Control.Concurrent.MVar
-import Control.Monad (replicateM_)
-
 import Control.Concurrent.Chan.Unagi.NoBlocking.Unboxed
+import Control.Concurrent.MVar (putMVar)
+import Control.Monad (replicateM_)
 
 import Common
 
@@ -13,8 +12,8 @@ import Common
 main :: IO ()
 main = spsc newChan (producer . fst) (\(_i, o) -> consumer o)
   where
-    producer i = replicateM_ iTERATIONS (writeChan i (1 :: Int))
+    producer i = replicateM_ iTERATIONS (writeChan i vALUE_TO_WRITE)
 
     consumer o consumerFinished = do
-      replicateM_ iTERATIONS (readChan (threadDelay 1) o)
+      replicateM_ iTERATIONS (readChan (threadDelay sLEEP_TIME) o)
       putMVar consumerFinished ()
