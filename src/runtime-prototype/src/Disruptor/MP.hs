40b51f8b562f333f320bbbc9070fd9188dfea277
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2021-11-03 14:33:44 +0100

    refactor(runtime): remove disruptor as it's now in its own repo

--- src/runtime-prototype/src/Disruptor/MP.hs
+++
@@ -1,6 +0,0 @@
-module Disruptor.MP (module X) where
-
-import Disruptor.MP.Consumer as X
-import Disruptor.MP.Producer as X
-import Disruptor.MP.RingBuffer as X
-import Disruptor.SequenceNumber as X
--- src/runtime-prototype/src/Disruptor/SP.hs
+++
@@ -1,6 +0,0 @@
-module Disruptor.SP (module X) where
-
-import Disruptor.SP.Consumer as X
-import Disruptor.SP.Producer as X
-import Disruptor.SP.RingBuffer as X
-import Disruptor.SequenceNumber as X
