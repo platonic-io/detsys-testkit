e05a081314c9737b9af6c04e20ad80f10b212de1
symbiont-stevan-andjelkovic <54943526+symbiont-stevan-andjelkovic@users.noreply.github.com>
2021-12-30 10:13:07 +0100

    Update src/journal/src/Journal/Internal/ByteBuffer.hs
    Co-authored-by: Daniel Gustafsson <60788891+symbiont-daniel-gustafsson@users.noreply.github.com>

--- src/journal/src/Journal/Internal/ByteBuffer.hs
+++ src/journal/src/Journal/Internal/ByteBuffer.hs
@@ -289,8 +289,8 @@ getByteString bb len = do
 
 getLazyByteString :: ByteBuffer -> Int -> IO LBS.ByteString
 getLazyByteString bb len = do
-  bytes <- replicateM len (getByte bb)
-  return (LBS.packBytes bytes)
+  bs <- getByteString bb len
+  return (LBS.fromStrict bs)
 
 getByteStringAt :: ByteBuffer -> Int -> Int -> IO BS.ByteString
 getByteStringAt bb offset len = do
