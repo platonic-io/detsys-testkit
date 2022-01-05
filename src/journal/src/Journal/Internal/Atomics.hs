b8c1d38b6c977d304daa71b2329e694887c47d59
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2022-01-05 09:17:34 +0100

    feat(journal): rename module to Atomics and add bindings for the different byte sizes

---
+++ src/journal/src/Journal/Internal/Atomics.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
+module Journal.Internal.Atomics where
+
+import Foreign
+
+------------------------------------------------------------------------
+
+foreign import ccall unsafe "stdatomic.h __atomic_fetch_add_1"
+  c_atomic_fetch_add_1 :: Ptr Word8 -> Word8 -> IO Word8
+
+foreign import ccall unsafe "stdatomic.h __atomic_fetch_add_2"
+  c_atomic_fetch_add_2 :: Ptr Word16 -> Word16 -> IO Word16
+
+foreign import ccall unsafe "stdatomic.h __atomic_fetch_add_4"
+  c_atomic_fetch_add_4 :: Ptr Word32 -> Word32 -> IO Word32
+
+foreign import ccall unsafe "stdatomic.h __atomic_fetch_add_8"
+  c_atomic_fetch_add_8 :: Ptr Word64 -> Word64 -> IO Word64
+
+fetchAddWord8Ptr :: Ptr Word8 -> Word8 -> IO Word8
+fetchAddWord8Ptr = c_atomic_fetch_add_1
+
+fetchAddWord16Ptr :: Ptr Word16 -> Word16 -> IO Word16
+fetchAddWord16Ptr = c_atomic_fetch_add_2
+
+fetchAddWord32Ptr :: Ptr Word32 -> Word32 -> IO Word32
+fetchAddWord32Ptr = c_atomic_fetch_add_4
+
+fetchAddWord64Ptr :: Ptr Word64 -> Word64 -> IO Word64
+fetchAddWord64Ptr = c_atomic_fetch_add_8
+
+------------------------------------------------------------------------
+
+t3 :: IO ()
+t3 = do
+  ptr <- callocBytes 1
+  zero <- fetchAddWord8Ptr ptr 2
+  print zero
+  two <- peekByteOff ptr 0 :: IO Word
+  print two
