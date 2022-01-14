87dde3be60637ba5c2b1afb0566f786db72a39e1
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2022-01-14 11:40:22 +0100

    fix(journal): check minBounds when casting

--- src/journal/src/Journal/Internal/Utils.hs
+++ src/journal/src/Journal/Internal/Utils.hs
@@ -36,16 +36,28 @@ withRWFd fp k =
     k
 
 int2Int32 :: Int -> Int32
-int2Int32 i@(I# i#) = assert (i <= fromIntegral (maxBound :: Int32)) (I32# i#)
+int2Int32 (I# i#) =
+  assert (fromIntegral (minBound :: Int32) <= I64# i#
+          && I64# i# <= fromIntegral (maxBound :: Int32))
+    (I32# i#)
 
 int322Int :: Int32 -> Int
-int322Int (I32# i#) = assert (I# i# <= (maxBound :: Int)) (I# i#)
+int322Int (I32# i#) =
+  assert (fromIntegral (minBound :: Int) <= I64# i#
+          && I64# i# <= fromIntegral (maxBound :: Int))
+    (I# i#)
 
 int2Int64 :: Int -> Int64
-int2Int64 i@(I# i#) = assert (i <= fromIntegral (maxBound :: Int64)) (I64# i#)
+int2Int64 (I# i#) =
+  assert ((minBound :: Int64) <= I64# i#
+          && I64# i# <= (maxBound :: Int64))
+    (I64# i#)
 
 int642Int :: Int64 -> Int
-int642Int i@(I64# i#) = assert (i <= fromIntegral (maxBound :: Int)) (I# i#)
+int642Int i@(I64# i#) =
+  assert (fromIntegral (minBound :: Int) <= i
+           && i <= fromIntegral (maxBound :: Int))
+    (I# i#)
 
 word322Int :: Word32 -> Int
 word322Int w@(W32# w#) = assert (W64# w# <= fromIntegral (maxBound :: Int)) (fromIntegral w)
