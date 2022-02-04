2df50b9b9cd3aa15cae4dde02efbf8caba4715af
Daniel Gustafsson <daniel.gustafsson@symbiont.io>
2022-02-04 16:54:43 +0100

    feat(sut): Dumblog get commands working
    * The Frontend will now parse the commands in the following way
      - A `POST` request is seen as a write
      - A `GET` request at /i is seen as a read at i

--- src/sut/dumblog/src/Types.hs
+++ src/sut/dumblog/src/Types.hs
@@ -1,10 +1,16 @@
+{-# LANGUAGE DeriveGeneric #-}
 module Types where
 
+import Data.Binary (Binary)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as LBS
+import GHC.Generics (Generic)
 
 data Command
   = Write ByteString
   | Read Int
+  deriving Generic
+
+instance Binary Command where
 
 type Response = LBS.ByteString
