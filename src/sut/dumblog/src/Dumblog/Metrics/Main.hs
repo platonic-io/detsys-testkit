ec14931eb0e35fc2e6fa7d513f5de7b7f38cda0f
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2022-03-10 11:16:58 +0100

    fix(sut): increase wake up retries

--- src/sut/dumblog/src/Dumblog/Metrics/Main.hs
+++ src/sut/dumblog/src/Dumblog/Metrics/Main.hs
@@ -2,6 +2,7 @@
 
 module Dumblog.Metrics.Main where
 
+import Data.Maybe (fromMaybe)
 import Control.Monad (forever)
 import Control.Concurrent (threadDelay)
 import Text.Printf (printf)
@@ -25,13 +26,13 @@ metricsMain = forever $ do
   m9999 <- percentile metrics ServiceTime 99.99
   mMax  <- percentile metrics ServiceTime 100
   putStrLn "Service time:"
-  putStrLn (maybe "N/A" (printf "  min   %10.2f µs") mMin)
-  putStrLn (maybe "N/A" (printf "  med   %10.2f µs") mMed)
-  putStrLn (maybe "N/A" (printf "  90    %10.2f µs") m90)
-  putStrLn (maybe "N/A" (printf "  99    %10.2f µs") m99)
-  putStrLn (maybe "N/A" (printf "  99.9  %10.2f µs") m999)
-  putStrLn (maybe "N/A" (printf "  99.99 %10.2f µs") m9999)
-  putStrLn (maybe "N/A" (printf "  max   %10.2f µs") mMax)
+  printf "  min   %10.2f µs\n" (fromMaybe 0 mMin)
+  printf "  med   %10.2f µs\n" (fromMaybe 0 mMed)
+  printf "  90    %10.2f µs\n" (fromMaybe 0 m90)
+  printf "  99    %10.2f µs\n" (fromMaybe 0 m99)
+  printf "  99.9  %10.2f µs\n" (fromMaybe 0 m999)
+  printf "  99.99 %10.2f µs\n" (fromMaybe 0 m9999)
+  printf "  max   %10.2f µs\n" (fromMaybe 0 mMax)
   cnt <- count metrics ServiceTime
   putStrLn (printf "  count %10d" cnt)
   threadDelay 1_000_000
