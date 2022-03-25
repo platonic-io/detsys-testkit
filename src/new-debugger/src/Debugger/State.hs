18d0c68129d0b5ca224333ea718d15e1fa39dadb
Daniel Gustafsson <daniel.gustafsson@symbiont.io>
2022-03-25 13:22:24 +0100

    feat(new-debugger): Generate sequence diagram from the debug-file

--- src/new-debugger/src/Debugger/State.hs
+++ src/new-debugger/src/Debugger/State.hs
@@ -4,8 +4,10 @@ module Debugger.State where
 import Data.Aeson
 import Data.Int
 import GHC.Generics (Generic)
-
 import Data.Vector (Vector)
+import qualified Data.Vector as Vector
+
+import Debugger.SequenceDia (Arrow(..), generate)
 
 data DebEvent = DebEvent
   { from :: String
@@ -40,16 +42,30 @@ instance FromJSON InstanceStateRepr
 
 instance ToJSON InstanceStateRepr
 
--- this should build the seqDia etc.
-fromRepr :: Vector InstanceStateRepr -> Vector InstanceState
-fromRepr = fmap repr
+toArrowDE :: DebEvent -> Arrow String
+toArrowDE de = Arrow
+  { aFrom = from de
+  , aTo = to de
+  , aAt = receivedLogical de
+  , aMessage = event de
+  }
+
+toArrowIS :: InstanceStateRepr -> [Arrow String]
+toArrowIS is = toArrowDE (currentEvent is) : map toArrowDE' (sent is)
   where
-    repr :: InstanceStateRepr -> InstanceState
-    repr i = InstanceState
+    -- we don't want to select the sent messages
+    toArrowDE' d = toArrowDE d{receivedLogical = -1}
+
+fromRepr :: Vector InstanceStateRepr -> Vector InstanceState
+fromRepr xs = Vector.imap repr xs
+  where
+    arrows = concatMap toArrowIS $ Vector.toList xs
+    repr :: Int -> InstanceStateRepr -> InstanceState
+    repr currentIx i = InstanceState
       { isState = state i
       , isCurrentEvent = currentEvent i
       , isRunningVersion = runningVersion i
-      , isSeqDia = "Sequence Diagram not supported yet!" -- should actually be built here
+      , isSeqDia = generate arrows currentIx
       , isLogs = logs i
       , isSent = sent i
       }
