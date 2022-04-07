e58b721738fdb7547f3b28cc9c4560d17a87431c
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2022-04-07 12:00:44 +0200

    fix(course): remove req from sm monad

--- doc/advanced-testing-mini-course/src/ATMC/Lec5/StateMachineDSL.hs
+++ doc/advanced-testing-mini-course/src/ATMC/Lec5/StateMachineDSL.hs
@@ -18,29 +18,29 @@ import ATMC.Lec5.Time
 
 ------------------------------------------------------------------------
 
-type SMM s req msg resp a = StateT s (Writer [Output resp msg]) a
+type SMM s msg resp a = StateT s (Writer [Output resp msg]) a
 
-runSMM :: SMM s req msg resp () -> s -> ([Output resp msg], s)
+runSMM :: SMM s msg resp () -> s -> ([Output resp msg], s)
 runSMM m s = (outputs, s')
   where
     (((), s'), outputs) = runWriter (runStateT m s)
 
-send :: NodeId -> msg -> SMM s req msg resp ()
+send :: NodeId -> msg -> SMM s msg resp ()
 send nid msg = lift (tell [InternalMessageOut nid msg])
 
-respond :: ClientId -> resp -> SMM s req msg resp ()
+respond :: ClientId -> resp -> SMM s msg resp ()
 respond cid resp = lift (tell [ClientResponse cid resp])
 
-set :: forall f s a req msg resp. HasField f s a
-    => a -> SMM s req msg resp ()
+set :: forall f s a msg resp. HasField f s a
+    => a -> SMM s msg resp ()
 set x = modify (\s -> setField @f s x)
 
-get :: forall f s a req msg resp. HasField f s a
-    => SMM s req msg resp a
+get :: forall f s a msg resp. HasField f s a
+    => SMM s msg resp a
 get = gets (getField @f)
 
-update :: forall f s a req msg resp. HasField f s a
-       => (a -> a) -> SMM s req msg resp ()
+update :: forall f s a msg resp. HasField f s a
+       => (a -> a) -> SMM s msg resp ()
 update u = modify (\s -> setField @f s (u (getField @f s)))
 
 data ExampleState = ExampleState
@@ -54,8 +54,8 @@ initExState = ExampleState 0
 instance HasField "esInt" ExampleState Int where
   hasField (ExampleState i) = (ExampleState, i)
 
-data Req  = Req
-  deriving Show
+data Req = Req
+  deriving (Eq, Show)
 
 data Msg = Msg
   deriving (Eq, Show)
@@ -63,7 +63,7 @@ data Msg = Msg
 data Resp = Resp Int
   deriving (Eq, Show)
 
-example :: Input Req Msg -> SMM ExampleState Req Msg Resp ()
+example :: Input Req Msg -> SMM ExampleState Msg Resp ()
 example (ClientRequest at cid req) = do
   set    @"esInt" 1
   update @"esInt" (+2)
