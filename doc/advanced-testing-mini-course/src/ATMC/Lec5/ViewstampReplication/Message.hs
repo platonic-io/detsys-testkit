a9a3d78d17e7b8f88c664bbd33eca2873e79be35
Daniel Gustafsson <daniel.gustafsson@symbiont.io>
2022-04-12 12:45:16 +0200

    feat(course): Start implementing client request for VRR

--- doc/advanced-testing-mini-course/src/ATMC/Lec5/ViewstampReplication/Message.hs
+++ doc/advanced-testing-mini-course/src/ATMC/Lec5/ViewstampReplication/Message.hs
@@ -5,7 +5,7 @@ module ATMC.Lec5.ViewstampReplication.Message where
 import ATMC.Lec5.StateMachine
 
 newtype RequestNumber = RequestNumber Int
-  deriving newtype Num
+  deriving newtype (Eq, Num, Ord)
 newtype ViewNumber = ViewNumber Int
   deriving newtype Num
 newtype OpNumber = OpNumber Int
