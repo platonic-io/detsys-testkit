8cf331f52e6954d4d0959cdd851a27d5fc072973
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2022-04-22 11:22:24 +0200

    feat(course): implement blackbox history

--- doc/advanced-testing-mini-course/src/ATMC/Lec5/History.hs
+++ doc/advanced-testing-mini-course/src/ATMC/Lec5/History.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 
 module ATMC.Lec5.History where
 
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TQueue
+import Data.Typeable
 
 import ATMC.Lec2ConcurrentSMTesting (History', Operation'(..), Pid(Pid))
 import qualified ATMC.Lec2ConcurrentSMTesting as Lec2
@@ -15,8 +18,9 @@ import ATMC.Lec5.StateMachine
 newtype History = History (TQueue HistEvent)
 
 data HistEvent = forall state req msg resp.
-  (Show state, Show req, Show msg, Show resp) =>
-  HistEvent NodeId state (Input req msg) state [Output resp msg]
+  ( Show state, Show req, Show msg, Show resp
+  , Typeable req, Typeable resp, Typeable msg
+  ) => HistEvent NodeId state (Input req msg) state [Output resp msg]
 deriving instance Show HistEvent
 
 newHistory :: IO History
@@ -32,16 +36,21 @@ readHistory (History q) = atomically (flushTQueue q)
 
 ------------------------------------------------------------------------
 
--- XXX: Remove existentials or add typeable constraints...
-{-
-data JepsenHistory = forall req resp. JepsenHistory (History' resp resp)
-
-blackboxHistory :: [HistEvent] -> JepsenHistory
-blackboxHistory = JepsenHistory . Lec2.History . go []
+blackboxHistory :: forall req resp msg. (Typeable req, Typeable resp, Typeable msg)
+                => [HistEvent] -> History' req resp
+blackboxHistory = Lec2.History . go []
   where
+    go :: [Operation' req resp] -> [HistEvent] -> [Operation' req resp]
     go acc [] = reverse acc
     go acc (HistEvent _nodeId _state input _state' outputs : evs) =
-      go (clientRequest input ++ concatMap clientResponse outputs ++ acc) evs
+      case (cast input, gcast outputs) of
+        (Just input', Just outputs') ->
+          go (clientRequest input' ++ concatMap clientResponse outputs' ++ acc) evs
+        (Just input', Nothing) ->
+          go (clientRequest input' ++ acc) evs
+        (Nothing, Just outputs') ->
+          go (concatMap clientResponse outputs' ++ acc) evs
+        _otherwise -> go acc evs
 
     clientRequest :: Input req msg -> [Operation' req resp]
     clientRequest (ClientRequest _time clientId req) = [Invoke (clientIdToPid clientId) req]
@@ -53,5 +62,3 @@ blackboxHistory = JepsenHistory . Lec2.History . go []
 
     clientIdToPid :: ClientId -> Pid
     clientIdToPid (ClientId cid) = Pid cid
-
--}
