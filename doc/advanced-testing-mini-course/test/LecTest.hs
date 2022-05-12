37076bd55ba347d958dcaa427ca594783d6ea03b
Stevan Andjelkovic <stevan.andjelkovic@symbiont.io>
2022-05-12 12:26:16 +0200

    feat(course): make faults into one-shots

--- doc/advanced-testing-mini-course/test/LecTest.hs
+++ doc/advanced-testing-mini-course/test/LecTest.hs
@@ -6,6 +6,3 @@ import qualified Lec04FaultInjection as Lec04
 
 unit_faultTest :: IO ()
 unit_faultTest = Lec04.unit_faultTest
-
-unit_singleWrite :: IO ()
-unit_singleWrite = Lec04.unit_singleWrite
